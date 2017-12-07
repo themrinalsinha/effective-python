2053117bdcda5c718a17aa039b5f352182549516
Mrinal Sinha <mail@themrinalsinha.com>
2017-12-08 02:59:52 +0530

    Item 2 : Follow the PEP 8 style guide

---
+++ item_2.py
@@ -0,0 +1,9 @@
+# Item 2 : Follow the PEP 8 style guide
+
+# NOTES:
+# PEP  = Python Enhancement Proposal
+# Read = https://www.python.org/dev/peps/pep-0008/
+
+# Always follow the PEP 8 style guide when writing python code.
+# Sharing a common style with the larger python community facilitates collaboration with others.
+# Using a consistent styles makes it easier to modify your own code later.
