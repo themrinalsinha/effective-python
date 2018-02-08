a0b6cd7b916baf0c4087f63d6db96911f19f8333
Mrinal Sinha <themrinalsinha@gmail.com>
2018-02-08 23:11:32 +0530

    Item - 22 : bySubjectGradebook

--- item_22.py
+++ item_22.py
@@ -27,3 +27,38 @@ book.report_grade('Mrinal', 80)
 book.report_grade('Mrinal', 50)
 book.report_grade('Mrinal', 10)
 print(book.average_grade('Mrinal'))
+
+# Dictionaries are so easy to use that there's a danger of overextending them to write brittle code.
+# Eg: say you want to extend the SimpleGradebook to keep a list of grades by subjects, not just over all.
+# you can do this by changing the grades dictionary to map student names (keys) to yet another dictionay (the values). 
+# The inner most dictionary will map subjects (the keys) to grades (the values) 
+
+class BySubjectGradebook(object):
+    def __init__(self):
+        self.grades = {}
+
+    def add_student(self, name):
+        self.grades[name] = {}
+
+    def report_grade(self, name, subject, grade):
+        by_subject = self.grades[name]
+        grade_list = by_subject.setdefault(subject, [])
+        grade_list.append(grade)
+
+    def average_grade(self, name):
+        by_subject   = self.grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
+
+book1 = BySubjectGradebook()
+book1.add_student('Warlock')
+book1.report_grade('Warlock', 'Math', 75)
+book1.report_grade('Warlock', 'Math', 85)
+book1.report_grade('Warlock', 'Science', 55)
+book1.report_grade('Warlock', 'Science', 85)
+book1.report_grade('Warlock', 'English', 55)
+book1.report_grade('Warlock', 'English', 96)
+print(book1.average_grade('Warlock'))
