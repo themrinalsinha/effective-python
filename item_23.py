da0ce7d63229b6f7937cc6b1e332015b515c47bc
Mrinal Sinha <themrinalsinha@gmail.com>
2018-02-10 09:25:53 +0530

    Item 23 - default dict

--- item_23.py
+++ item_23.py
@@ -8,3 +8,24 @@ names = ['Socrates', 'Archimedes', 'Plato', 'Aristotle']
 names.sort(key = lambda x : len(x))
 print(names)
 
+# Eg: say you want to customize the behavior of the defaultdict class. The data structure allows yo uto supply a function that will be caled each time a missing key is accessed.
+# The function must return the default value the missing key is accessed. The function must return the default value the missing key should have in the dictionary.
+# Here I define a hook that logs each time a key is missing and returns 0 for the default value:
+from collections import defaultdict
+
+def log_missing():
+    print('Key added')
+    return 0
+
+current    = {'green' : 12, 'blue' : 3}
+increments = [
+    ('red', 5),
+    ('blue', 17),
+    ('orange', 9),
+]
+
+result = defaultdict(log_missing, current)
+print('Before : ', dict(result))
+for key, amount in increments:
+    result[key] += amount
+print('After : ', dict(result))
