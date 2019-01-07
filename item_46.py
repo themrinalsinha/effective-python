dd239d2c077c6dbadd2749aa26b0279d9d650807
Mrinal Sinha <themrinalsinha@gmail.com>
2019-01-08 01:56:22 +0530

    Ordered dictionary

--- item_46.py
+++ item_46.py
@@ -23,5 +23,46 @@ y = fifo.pop()     # Consumer - Pop element from right
 # You can insert or remove items from the end of a list in constant time.
 # but inserting or removing itmes from the head fo a list takes linear time.
 # which is much slower than the constant tie of a deque.
+# ================================================================================
 
+# ORDERED DICTIONARY
+# Standard dictionaries are unordered. That means a dict with a same key and values
+# can result in different orders of iterations. This behavior is a surprising by product
+# of the way the dictionary's fast hash table is implemeted.
 
+a = {}
+a['foo'] = 1
+a['bar'] = 2
+
+# Randomly populate b to cause hash conflicts.
+from random import randint
+while True:
+    z = randint(99, 1013)
+    b = {}
+    for i in range(z):
+        b[i] = i
+    b['foo'] = 1
+    b['bar'] = 2
+
+    for i in range(z):
+        del b[i]
+    if str(b) != str(a):
+        break
+print(a)
+print(b)
+
+# The OrderedDict class from collections module is a special type of dictionary that keeps
+# track of the order in which itss keys were inserted. Iterating the keys of an OrderedDict
+# has predictable behavior. This can vastly simplify testingg and debbing by making all code deterministic.
+
+from collections import OrderedDict
+
+a = OrderedDict()
+a['foo'] = 1
+a['bar'] = 2
+print(a)
+b = OrderedDict()
+b['foo'] = 'red'
+b['bar'] = 'blue'
+for v1, v2 in zip(a.values(), b.values()):
+    print(v1, v2)
