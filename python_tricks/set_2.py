c9ba494b17cf0799c4b82afd46a60c41e05c2ee0
Mrinal Sinha <themrinalsinha@gmail.com>
2019-01-10 18:31:30 +0530

    old style string formatting

---
+++ python_tricks/set_2.py
@@ -0,0 +1,17 @@
+# String formatting
+
+# Old style string formatting
+name = 'Mrinal'
+print('Hello %s' % name)
+
+# %x format specifier to convert an int value to a string and
+# represent it as a hexidecimal number:
+errno = 50159747054
+print('%x' % errno)
+
+print('Hey %s, there is 0x%x error!' % (name, errno))
+# It's also possible to refer to variable substitutions by name in your format
+# string, if you pass a mapping to the %-operator:
+
+print('Hey %(name)s, there is Ox%(errno)s error!' % {"name": name, "errno": errno})
+# This makes your format strings easier to maintain and easier to modify in the future.
