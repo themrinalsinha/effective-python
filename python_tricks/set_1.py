55f3efc1fe8b9a378fd337c3600d32b1a1b21cb9
Mrinal Sinha <themrinalsinha@gmail.com>
2019-01-10 14:32:32 +0530

    handling missing comma in list

---
+++ python_tricks/set_1.py
@@ -0,0 +1,12 @@
+# If you miss a comma between two list items,
+# list is going to join them, this is known as
+# "string literal concatenation"
+data_1 = ['Alice', 'Bob', 'Dilbert', 'Jane']
+data_2 = [
+    'Alice',
+    'Bob'
+    'Delbert',
+    'Jane'
+]
+print(data_1)
+print(data_2)
