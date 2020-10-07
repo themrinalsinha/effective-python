a6f695d3640bc6b9fffa440b8623fd1f801c21c3
Mrinal Sinha <themrinalsinha@gmail.com>
2020-10-07 10:43:50 +0530

    basics of metaprogramming

---
+++ cookbook/CH9_Metaprogramming/script_1.py
@@ -0,0 +1,20 @@
+"""
+One of the most important mantras of software development is "Don't repeat yourself".
+That is, any time you are facing problem with creating highly repetitive code, it often
+pays to look for a more elegant solution. In Python, such problems are often solved under
+the category of "metaprogramming"
+
+In a nutshell, metagrogramming is about creating functions and classes whose main goal is
+to manipulate code (eg. modifying, generating or wrapping existing code).
+
+Main features include:
+- decorators
+- class decorators
+- metaclasses
+and other useful topics -- including signature objects, execution of code with exec() and
+inspecting the internals of functions and classes.
+"""
+
+"""
+
+"""
