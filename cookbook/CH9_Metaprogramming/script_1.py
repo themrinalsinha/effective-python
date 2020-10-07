4dc81db4513c15a2f3d3e115cd2eeeb50b16bbd1
Mrinal Sinha <themrinalsinha@gmail.com>
2020-10-07 10:52:00 +0530

    9.1. Putting a wrapper around a function

--- cookbook/CH9_Metaprogramming/script_1.py
+++ cookbook/CH9_Metaprogramming/script_1.py
@@ -16,5 +16,63 @@ inspecting the internals of functions and classes.
 """
 
 """
+9.1. Putting a wrapper around a function
 
+Problem: you want to put a wrapper layer around that adds extra processing (eg: logging, timing etc)
+
+Solution: decorator function
+"""
+
+import time
+from functools import wraps
+
+def timethis(func):
+    """
+    decorator that reports the execution time.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end-start)
+        return result
+    return wrapper
+
+@timethis
+def countdown(n):
+    """
+    counts down
+    """
+    while n > 0:
+        n -= 1
+
+countdown(1000000)
+countdown(100000000)
+
+"""
+A decorator is a function that accepts a function as input and returns a new function
+as output, whenever we write code like this:
+
+    @timethis
+    def countdown(n):
+        ....
+        ....
+
+it is same as if you had performed these separate steps:
+
+    countdown = timethis(countdown)
+"""
+
+"""
+Aa an aside, built-in decorators such as @staticmethod, @classmethod and @property
+works in the same way.
+
+    class A:
+        @classmethod
+        def method(cls):
+            pass
+
+is equivalent to
+    method = classmethod(method)
 """
