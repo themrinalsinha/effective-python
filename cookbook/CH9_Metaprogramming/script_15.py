1505d58b1102959b5f609914ea5fcc53d2eed23f
Mrinal Sinha <themrinalsinha@gmail.com>
2020-12-03 16:17:04 +0530

    9.20. Implementing Multiple Dispatch with Function Annotations

--- cookbook/CH9_Metaprogramming/script_15.py
+++ cookbook/CH9_Metaprogramming/script_15.py
@@ -167,3 +167,49 @@ s = Spam()
 s.bar(2, 3)
 s.bar('Hello')
 s.bar('Hello', 5)
+print('-' * 50)
+# ======================================================
+
+import types
+
+class multimethod:
+    def __init__(self, func) -> None:
+        self._methods = {}
+        self.__name__ = func.__name__
+        self._default = func
+
+    def match(self, *types):
+        def register(func):
+            ndefaults = len(func.__defaults__) if func.__defaults__ else 0
+            for n in range(ndefaults+1):
+                self._methods[types[:len(types) - n]] = func
+            return self
+        return register
+
+    def __call__(self, *args):
+        types = tuple(type(arg) for arg in args[1:])
+        meth = self._methods.get(types, None)
+        if meth:
+            return meth(*args)
+        else:
+            return self._default(*args)
+
+    def __get__(self, instance, cls):
+        if instance is not None:
+            return types.MethodType(self, instance)
+        else:
+            return self
+
+class Spam:
+    @multimethod
+    def bar(self, *args):
+        #default method called if no match
+        raise TypeError("No matching method for bar")
+
+    @bar.match(int, int)
+    def bar(self, x, y):
+        print(f"Bar 1: {x}, {y}")
+
+    @bar.match(str, int)
+    def bar(self, s, n=0):
+        print(f"Bar 2: {s}, {n}")
