5557450cde4ec254c0e74299982d9c1407a119bd
Mrinal Sinha <themrinalsinha@gmail.com>
2020-12-03 13:19:18 +0530

    9.20. Implementing Multiple Dispatch with Function Annotations

--- cookbook/CH9_Metaprogramming/script_15.py
+++ cookbook/CH9_Metaprogramming/script_15.py
@@ -10,6 +10,7 @@ Solution: Performing initialization or setup actions at the time of class defini
 """
 
 import operator
+import typing
 
 class StructTupleMeta(type):
     def __init__(cls, *args, **kwargs) -> None:
@@ -32,3 +33,137 @@ class Point(StructTuple):
 
 s = Stock("ACME", 50, 91.1)
 print(s)
+print('- ' * 50)
+# ==================================================================================
+
+"""
+9.20. Implementing Multiple Dispatch with Function Annotations
+
+Problem: You've learned function argument annotations and you have a thought that you
+         might be able to use them to implement multiple-dispatch (method overloading)
+         based on types. However, you're not quite sure what's involved (or if it's even a good idea)
+
+Solution: This recipe is based on a simple observation -- namely, that since Python allows
+          arguments to be annotated, perhaps it might be possible to write code like this:
+"""
+class Spam:
+    def bar(self, x: int, y: int):
+        print("Bar 1: ", x, y)
+    def bar(self, s: str, n: int=0):
+        print("Bar 2: ", s, n)
+
+s = Spam()
+s.bar(2, 3)    # prints Bar 1: 2 3
+s.bar("Hello") # prints Bar 2: hello 0
+
+# Here is the start of a solution that does just that, using a combination of metclass
+# and descriptors
+
+import inspect
+import types
+
+class MultiMethod:
+    """
+    Represents a single multimethod
+    """
+    def __init__(self, name) -> None:
+        self._methods = {}
+        self.__name__ = name
+
+    def register(self, meth):
+        """
+        Register a new method as a multimethod
+        """
+        sig = inspect.signature(meth)
+        # build a type signature from the method's annotations
+        types = []
+        for name, parm in sig.parameters.items():
+            if name == "self":
+                continue
+            if parm.annotation is inspect.Parameter.empty:
+                raise TypeError(
+                    f"Argument {name} must be annotated with a type"
+                )
+            if not isinstance(parm.annotation, type):
+                raise TypeError(
+                    f"Argument {name} annotation must be a type"
+                )
+            if parm.default is not inspect.Parameter.empty:
+                self._methods[tuple(types)] = meth
+            types.append(parm.annotation)
+
+        self._methods[tuple(types)] = meth
+
+    def __call__(self, *args):
+        """
+        Call a method based on type signature of the arguments
+        """
+        types = tuple(type(arg) for arg in args[1:])
+        meth = self._methods.get(types, None)
+        if meth:
+            return meth(*args)
+        else:
+            raise TypeError(f"No matching method for types {types}")
+
+    def __get__(self, instance, cls):
+        """
+        Descriptor method needed to make calls work in a class
+        """
+        if instance is not None:
+            return types.MethodType(self, instance)
+        else:
+            return self
+
+class MultiDict(dict):
+    """
+    Special dictionary to build multimethods in a metaclass
+    """
+    def __setitem__(self, key, value) -> None:
+        if key in self:
+        # if key already exists, it must be a multimethod or callable
+            current_value = self[key]
+            if isinstance(current_value, MultiMethod):
+                current_value.register(value)
+            else:
+                mvalue = MultiMethod(key)
+                mvalue.register(current_value)
+                mvalue.register(value)
+                super().__setitem__(key, mvalue)
+        else:
+            super().__setitem__(key, value)
+
+class MultipleMeta(type):
+    """
+    Metaclass that allows multiple dispatch of methods
+    """
+    def __new__(cls, clsname, bases, clsdict):
+        return type.__new__(cls, clsname, bases, dict(clsdict))
+
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return MultiDict()
+
+# To use this class, you write code like this:
+class Spam(metaclass=MultipleMeta):
+    def bar(self, x:int, y:int):
+        print(f"Bar 1: {x}, {y}")
+    def bar(self, s:str, n:int=0):
+        print(f"Bar 2: {s}, {n}")
+
+# Example: overloaded __init__
+import time
+
+class Date(metaclass=MultipleMeta):
+    def __init__(self, year:int, month:int, day:int) -> None:
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __init__(self) -> None:
+        t = time.localtime()
+        self.__init__(t.tm_year, t.tm_mon, t.tm_mday)
+
+s = Spam()
+s.bar(2, 3)
+s.bar('Hello')
+s.bar('Hello', 5)
