4e6e76a939a6ccde6c5cd9880d617a1707786783
Mrinal Sinha <themrinalsinha@gmail.com>
2021-03-07 16:33:37 +0530

    3.7. Working with Infinity and NaNs

--- cookbook/CH3__Numbers_Dates_Times/script_2.py
+++ cookbook/CH3__Numbers_Dates_Times/script_2.py
@@ -22,5 +22,28 @@ print(x.to_bytes(16, 'little'))
 # networking. For instance, IPv6 network addresses are represented as 128-bit integers.
 # If you are writing code that needs to pull such values out of a data record, you might
 # face this problem.
-print()
+print('- ' * 50)
 # =====================================================================================
+
+"""
+3.7. Working with Infinity and NaNs
+
+Problems: You need to create or test for the floating-point values of infinity, negative infinity
+          or NaN (not a number).
+"""
+
+# Python has no special syntax to represent these special floating-point values, but they
+# can be created using float().
+
+a = float('inf')
+b = float('-inf')
+c = float('nan')
+
+print(a, b, c)
+# To test for the presence of these values, use the math.isinf() and math.isnan() functions.
+import math
+print(math.isinf(a))
+print(math.isinf(b))
+print(math.isinf(c))
+print(math.isnan(c))
+print('- ' * 50)
