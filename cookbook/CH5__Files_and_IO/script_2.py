b47f9a4574fda8288f89956fe7682c0aa970bba5
Mrinal Sinha <themrinalsinha@gmail.com>
2021-03-10 09:57:21 +0530

    5.3. Printing with a different separator or line ending

--- cookbook/CH5__Files_and_IO/script_2.py
+++ cookbook/CH5__Files_and_IO/script_2.py
@@ -4,3 +4,11 @@
 with open("somefile.txt", "w") as f:
     print("Hello world !", file=f)
 # ----------------------------------------------------------------------------------
+
+
+"""
+5.3. Printing with a different separator or line ending
+"""
+print("ACME", 50, 90.5)
+print("ACME", 50, 90.5, sep=',')
+print("ACME", 50, 90.5, sep=',', end="!!\n")
