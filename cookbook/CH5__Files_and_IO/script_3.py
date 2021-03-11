3a2b48f27c9c734aafbb4274cca30f93c034c962
Mrinal Sinha <themrinalsinha@gmail.com>
2021-03-11 12:44:01 +0530

    5.9. Reading Binary Data into a Mutable Buffer

--- cookbook/CH5__Files_and_IO/script_3.py
+++ cookbook/CH5__Files_and_IO/script_3.py
@@ -11,7 +11,7 @@ RECORD_SIZE = 32
 with open("somefile.data", "rb") as f:
     records = iter(partial(f.read, RECORD_SIZE), b'')
     for r in records:
-        pass
+        print(r)
 
 # The records object in this example is an iterable that will produce fixed-sized
 # chunks until end of the file is reached. However, be aware that the last item may
@@ -23,3 +23,30 @@ with open("somefile.data", "rb") as f:
 # when a file is read but the end of file has been reached.
 print('- ' * 50)
 # ------------------------------------------------------------------------------------
+
+"""
+5.9. Reading Binary Data into a Mutable Buffer
+
+Problem: You want to read binary data directly into a mutable buffer without any intermediate
+         copying. Perhaps you want to mutate the data in-place and write it back out to a file.
+
+Solution: To read data into a mutable array, use the readinto() method of files
+"""
+import os
+
+def read_into_buffer(filename):
+    buf = bytearray(os.path.getsize(filename))
+    with open(filename, 'rb') as f:
+        f.readinto(buf)
+    return buf
+
+# here is an example of illustrating the usage
+with open("sample.bin", 'wb') as f:
+    f.write(b'Hello world')
+
+buf = read_into_buffer('sample.bin')
+print(buf)
+print(buf[0:5])
+
+with open('somefile.data', 'wb') as f:
+    f.write(buf)
